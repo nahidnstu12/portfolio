54f40d6a2f150626abde940adaf0ec71580997bc
= <=>
2021-03-07 11:25:08 +0600

    sidebar design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import {Sidebar} from '../components/Sidebar';
+import {Row, Col} from 'reactstrap'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Row className="p-5 bg-dark">
+		<Col sm="12" lg="3" className="p-4 text-center bg-white rounded my-2 mr-5"> 
+		<Sidebar />
+		</Col>
+		<Col sm="12" lg="8" className="text-center bg-white rounded my-2">
+			<Component {...pageProps} />
+		</Col>
+    </Row>
+  // 
+  )
 }
 
 export default MyApp
